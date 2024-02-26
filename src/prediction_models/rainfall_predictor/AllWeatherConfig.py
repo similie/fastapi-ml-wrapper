a74abf628b8600f947a809667de34b02c183258e
s-g-1 <github@softwareanvil.co.uk>
2024-02-26 23:15:54 +0100

    Update AllWeatherConfig.py

--- src/prediction_models/rainfall_predictor/AllWeatherConfig.py
+++ src/prediction_models/rainfall_predictor/AllWeatherConfig.py
@@ -7,7 +7,7 @@ class AllWeatherConfig(BaseSettings):
     # custom properties (provide defaults or environment vars)
     cube_name: str = 'all_weather'
     cube_restapi: str = 'http://localhost:4000/cubejs-api/v1/load'
-    cube_port: int
+    cube_port: int = 4000
 
 
 @lru_cache
