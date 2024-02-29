53669f9055a4d2be1f7b8bbd5f5638e6800ff7eb
s-g-1 <github@softwareanvil.co.uk>
2024-02-29 14:47:28 +0100

    wip: add test for cube json loader

---
+++ test/test_cubejs_query_response.py
@@ -0,0 +1,29 @@
+import json
+from os import path, getcwd
+from src.prediction_models.rainfall_predictor.AllWeatherConfig import getAllWeatherConfig
+from src.prediction_models.rainfall_predictor.AllWeatherCubeResponse import AllWeatherCubeQueryResponse
+from src.prediction_models.rainfall_predictor.AllWeatherCubeResponse import cleanCubeNameFromResponseKeys
+
+
+def loadJsonFixture():
+    '''
+    load the sample Json file to the Cube query resonse model format.
+    '''
+    p = path.join(getcwd(), 'test', 'fixtures', 'dataloader.json')
+    with open(p, 'r') as file:
+        jsonData = json.load(file)
+        return json.dumps(jsonData)
+
+
+def test_clean_cubename_from_response():
+    jsonData = loadJsonFixture()
+    assert jsonData is not None
+
+    cubeName = getAllWeatherConfig().cube_name
+    cleanedJson = cleanCubeNameFromResponseKeys(jsonData)
+    assert cleanedJson.find(cubeName) == -1
+
+    jsonData = json.loads(cleanedJson)
+    model = AllWeatherCubeQueryResponse.model_validate(jsonData)
+    assert model is not None
+    assert len(model.data) > 0
