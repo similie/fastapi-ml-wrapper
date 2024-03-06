ea4c4f2cc09e89f3791100e66ff0582d1a17e454
s-g-1 <github@softwareanvil.co.uk>
2024-03-06 17:51:41 +0100

    wip: add env vars for ml model

--- src/prediction_models/rainfall_predictor/AllWeatherConfig.py
+++ src/prediction_models/rainfall_predictor/AllWeatherConfig.py
@@ -11,9 +11,72 @@ class AllWeatherConfig(BaseSettings):
     cube_auth_key: str = ''
 
 
+class ExperimentConfig(BaseSettings):
+    '''
+    Experiments config settings, with .env namespace aliases
+    '''
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore', env_prefix='experiment_')
+    name: str
+    train_path: str
+    val_path: str
+    pred_path: str
+    check_path: str
+    lr: float
+    weight_decay: float
+    batch_size: int
+    sequence_length: int
+
+
+class LstmConfig(BaseSettings):
+    '''
+    LSTM config settings, with .env namespace aliases
+    '''
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore', env_prefix='lstm_')
+    name: str
+    n_features: int
+    hidden_size: int
+    sequence_length: int = 12
+    batch_size: int = 1
+    num_layers: int = 2
+    dropout: float = 0.5
+    prediction_window: int = 168
+
+
+class TrainerConfig(BaseSettings):
+    '''
+    Trainer config settings, with .env namespace aliases
+    '''
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore', env_prefix='trainer_')
+    precision: int
+    accelerator: str
+    default_root_dir: str
+    max_epochs: int
+    log_every_n_steps: int
+
+
+class AllWeatherMLConfig(BaseSettings):
+    '''
+    Namespaced ML config settings from .env
+    '''
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+    experiment_config: ExperimentConfig = ExperimentConfig()
+    lstm_config: LstmConfig = LstmConfig()
+    trainer_config: TrainerConfig = TrainerConfig()
+
+
 @lru_cache
 def getAllWeatherConfig():
     '''
     Lazy load all weather config
     '''
     return AllWeatherConfig()
+
+
+@lru_cache
+def getAllWeatherMLConfig():
+    '''
+    Lazy loader for All weather ML models. Note. In some cases, certain values
+    listed here might be overidden by input params received via the API. Check
+    the README.md in rainfall_predictor for variables and their default values.
+    '''
+    return AllWeatherMLConfig()
--- test/test_all_weather_config.py
+++ test/test_all_weather_config.py
@@ -1,4 +1,10 @@
-from src.prediction_models.rainfall_predictor.AllWeatherConfig import AllWeatherConfig, getAllWeatherConfig
+import pytest
+from src.prediction_models.rainfall_predictor.AllWeatherConfig import (
+    AllWeatherConfig,
+    getAllWeatherConfig,
+    AllWeatherMLConfig,
+    getAllWeatherMLConfig
+)
 
 
 def test_app_config():
@@ -6,12 +12,40 @@ def test_app_config():
     assert config is not None
 
 
-def test_get_config():
+def test_app_get_config():
     config = getAllWeatherConfig()
     assert config is not None
 
 
-def test_api_endpoint():
+def test_app_cube_api_settings():
     config = getAllWeatherConfig()
     assert len(config.cube_name) > 0
     assert config.cube_rest_api.startswith('http')
+
+
+def test_all_weather_ml_config():
+    config = AllWeatherMLConfig()
+    assert config is not None
+
+
+def test_all_weather_get_ml_config():
+    config = getAllWeatherMLConfig()
+    assert config is not None
+
+
+def test_all_weather_ml_config_submodels():
+    config = getAllWeatherMLConfig()
+    assert config.experiment_config is not None
+    assert config.lstm_config is not None
+    assert config.trainer_config is not None
+
+
+def test_all_weather_ml_config_settings():
+    config = getAllWeatherMLConfig()
+    assert config.experiment_config.batch_size == 1
+    assert config.experiment_config.sequence_length == 12
+    assert config.lstm_config.batch_size == 1
+    assert config.lstm_config.sequence_length == 12
+    # precision is e.g. 32, 64 etc.
+    assert pytest.approx(config.trainer_config.precision % 2) == 0
+    assert len(config.trainer_config.accelerator) > 0
