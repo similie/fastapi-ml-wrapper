0ce5655284b504de75effe7380e5e1a7f058ffff
Leigh Gable <leighgable@gmail.com>
2024-04-23 19:13:58 +0200

    added LSTMPredictor.py

--- src/prediction_models/LSTMPredictor.py
+++ src/prediction_models/LSTMPredictor.py
@@ -13,48 +13,105 @@ from .lstm_predictor.AllWeatherCubeRequest import AllWeatherQueryMeasures
 from .lstm_predictor.AllWeatherCubeResponse import AllWeatherQueryMeasuresResponse
 from .lstm_predictor.predict import predict
 
-class LstmTemplateProps(BaseModel):
-    '''
-    Properties model for LstmPredictor Template
-    '''
-    fieldOne: str = 'f1'
-    fieldTwo: int = 1
-
-
 class LSTMPredictor(BasePredictor):
     '''
-    Implementation class of abstract BasePredictor for testing
+    Implementation class of abstract BasePredictor with typed Payloads
     '''
+    def __init__(self, payload) -> None:
+        super().__init__(payload)
+
     async def template(self) -> TemplateResponse:
         t = await super().template()
-        t.notes = 'lstm predictor schema'
-        t.events = ['onTest']
-        t.accepts = LstmTemplateProps.model_json_schema()
+        t.notes = '''
+        Input properties are a list of weather data. `POST` requests return
+        immediately and results are supplied via the callback parameters
+        specified in the webhook
+        '''
+        t.events = []
+        t.accepts = AllWeatherQueryMeasures.model_json_schema()
         return t
 
-    # async def fineTune(self, payload: Any):
-    #     await super().fineTune(payload)
-    #     result = {
-    #         'result': True,
-    #         'count': 0,
-    #         'payload': payload
-    #     }
-    #     return result
+    async def __loadCubeJson(self, payload: CubePredictionPostRequest):
+        stations = payload.stations if payload.stations is not None else []
+        # TODO: other params: limit, skip TZ
+        json = loadJson(payload.dateRange, stations)
+        return json
 
-    async def predict(self, payload: Any, taskManager: BackgroundTasks | None = None):
-        await super().predict(payload)
-        result = {
-            'result': True,
-            'count': 0,
-            'payload': payload
-        }
-        return result
+    async def fineTune(self, payload: Any):
+        return super().fineTune(payload)
 
-    async def compute_model_error(self, payload: Any):
-        await super().compute_stochastic_dropout(payload)
-        result = {
-            'result': True,
-            'count': 0,
-            'payload': payload
-        }
-        return result
+    def guardPredictionPayload(
+            self,
+            payload: CubePredictionPostRequest | ForecastPredictionPostRequest
+            ) -> CubePredictionPostRequest | ForecastPredictionPostRequest:
+        '''
+        Attempts to validate the payload in the Predict pathway to one of the
+        expected types. Returns a validated model instance if payload can be
+        validated against either class or throws.
+        '''
+        noop = 0
+
+        # print('in Rainfall predictor, guardPayload')
+        # print(payload.model_dump_json(indent=4))
+        # print(f'keys: {payload.model_dump().keys()}')
+        modelDict = payload.model_dump()
+        # print(f'{modelDict['modelName']}')
+        # print(f'{modelDict['webhook']}')
+
+        # hook = WebhookRequest.model_validate(modelDict['webhook'])
+        # print(hook.model_dump())
+        # s = '{
+        #  "station":27,"avg_dew_point":8.69,"hour":"2024-02-29T13:00:00Z",
+        #  "date":"2024-02-29T13:00:00Z","avg_wind_direction":179.81,
+        #  "avg_wind_speed":6.19,"avg_soil_moisture":null,"avg_solar":null,
+        #  "avg_temperature":9.38,"avg_humidity":95,"avg_pressure":1007.38,
+        #  "sum_precipitation":0.32
+        #  }'
+        # qms = AllWeatherQueryMeasuresResponse.model_validate_json(s)
+        # print(qms.model_dump())
+
+        try:
+            model = CubePredictionPostRequest.model_validate(modelDict) # noqa F841
+            return model
+        except ValidationError:
+            noop = noop + 0
+
+        try:
+            model = ForecastPredictionPostRequest.model_validate(modelDict)
+            return model
+        except ValidationError:  # as exception
+            # print(exception.errors()[0]['type'])
+            raise
+
+    async def predict(
+            self,
+            payload: CubePredictionPostRequest | ForecastPredictionPostRequest,
+            taskManager: BackgroundTasks | None
+            ):
+        payloadModel = self.guardPredictionPayload(payload)
+        # TODO branch here for inference inline (or in background if webhook supplied)
+        # For straight inference, return a ForecastPredictionPostResponse. If a webhook
+        # was supplied, return a BackgroundTaskResponse and add result data to the
+        # WebhookResponse instance, returned via `sendWebhookIfNeeded`
+
+        data: list[AllWeatherQueryMeasuresResponse]
+        if isinstance(payloadModel, CubePredictionPostRequest):
+            cubeResult = await self.__loadCubeJson(payloadModel)
+            data = cubeResult.data
+        else:
+            data = payloadModel.data
+        # request Data is now either the input weather forcast or station measurements
+
+        pdData = [item.model_dump(by_alias=True) for item in data]
+        dataFrame = pd.DataFrame(pdData)
+        # pass [data] into model for inference
+        predictions = predict(
+            data=dataFrame,
+        )
+
+        return DataTaskResponse(
+            status=200,
+            message=f'Inference in {self.__class__.__name__},count:{len(predictions[0].data)}',
+            data=predictions
+        )
+
